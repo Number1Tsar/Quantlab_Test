eb5146db77d88df8735777ae63cfc1699b67c0c3
Sulav Adhikari <adhikari485@gmail.com>
2020-02-02 23:07:20 -0600

    Command line argument added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include "internals/TradeProcessor.h"
+#include "internals/pch.h"
 #include <ctime>
 
-int main()
+/*
+    First command line argument is input file location.
+    Second command line argument (optional) is output file location. default output filename is "output.csv"
+*/
+int main(int argc, char* argv[])
 {
+    if(argc < 2)
+    {
+        printf("Invalid command. Please provide command in following way. OuputFile argument is optional\n");
+        printf("./out <inputfile> <outputfile>");
+        exit(1);
+    }
+    std::string inputFileName = argv[1];
+    std::string outputFileName = "./output.csv";
+    if(argc >= 2) outputFileName = argv[2];
     const clock_t startTime = clock();
-    TradeProcessor processor("C:\\\\Users\\Sulav\\CLionProjects\\Quantlabs_Sulav_Adhikari\\input.csv");
+    TradeProcessor processor(inputFileName);
     processor.ProcessTrades();
-    processor.generateOutputToFile("C:\\\\Users\\Sulav\\CLionProjects\\Quantlabs_Sulav_Adhikari\\results\\output_dummy.csv");
+    processor.generateOutputToFile(outputFileName);
     auto timeTaken = float(clock() - startTime) / CLOCKS_PER_SEC;
     printf("Total time taken: %f\n", timeTaken);
     return 0;
