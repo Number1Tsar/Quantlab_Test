537ecede262888b057e9829defd0acc461afea0d
Sulav Adhikari <adhikari485@gmail.com>
2020-02-02 23:17:47 -0600

    Input file provided from argument

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "internals/pch.h"
 #include <ctime>
 
+
 /*
     First command line argument is input file location.
     Second command line argument (optional) is output file location. default output filename is "output.csv"
@@ -10,13 +11,13 @@ int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        printf("Invalid command. Please provide command in following way. OuputFile argument is optional\n");
-        printf("./out <inputfile> <outputfile>");
+        printf("Invalid command. Please provide command in following way. Ouput File argument is optional\n");
+        printf("./out <inputfile name> <outputfile name>");
         exit(1);
     }
     std::string inputFileName = argv[1];
     std::string outputFileName = "./output.csv";
-    if(argc >= 2) outputFileName = argv[2];
+    if(argc > 2) outputFileName = argv[2];
     const clock_t startTime = clock();
     TradeProcessor processor(inputFileName);
     processor.ProcessTrades();
